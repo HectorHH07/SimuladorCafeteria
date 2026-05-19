7a67da784de0df77cca31545e839ad99496685cf
Hector <hectorhh185@gmail.com>
2026-05-19 10:06:44 -0600

    Agregar archivos de proyecto.

---
+++ SimuladorCafeteria/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimuladorCafeteria
+{
+    internal class Bebida
+    {
+        private string nombre;
+        private string tamaño;
+        private double precio;
+        //constructor para inicializar los productos
+        public Bebida(string nombre, string tamaño, double precio)
+        {
+            this.nombre = nombre;
+            this.tamaño = tamaño;
+            this.precio = precio;
+        }
+        //Metodo para simular la preparacion
+        public void Preparar()
+        {
+            Console.WriteLine($"Preparando un {nombre} tamaño {tamaño}");
+        }
+        //Metodo para aplicar descuento
+        public void AplicarDescuento(double porcentaje)
+        {
+            if (porcentaje >0 && porcentaje <=100)
+            {
+                double descuento = precio * (porcentaje / 100);
+                precio=descuento;
+                Console.WriteLine($"Se aplico un {porcentaje}% de descuento a {nombre}");
+            }
+            else
+            {
+                Console.WriteLine("Porcentaje de descuento no aceptado");
+            }
+        }
+        //Metodo para mostrar descripcion
+        public void MostrarDescripcion()
+        {
+            Console.WriteLine($"Esto es un {nombre} de tamaño {tamaño} con un costo de ${precio}");
+        }
+    }
+}
---
+++ SimuladorCafeteria/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace SimuladorCafeteria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bebida[] bebidas = new Bebida[3];
+            
+            //solicitamos datos de la bebida
+            Console.WriteLine("Bebidas\n");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Bebida{i + 1}");
+
+                Console.Write("Nombre de la bebida:");
+                string nombre=Console.ReadLine();
+                Console.Write("Tamaño (Pequeño,Mediano,Grande):");
+                string tamaño = Console.ReadLine();
+                Console.Write("Precio:$");
+                double precio = Convert.ToDouble(Console.ReadLine());
+
+                bebidas[i] = new Bebida(nombre, tamaño, precio);
+                Console.WriteLine($"Bebida{i + 1} registrada con exito:\n");
+            }
+            //Aplicamos el descuneto
+            Console.WriteLine("Descuento");
+            bebidas[0].AplicarDescuento(15);
+            bebidas[1].AplicarDescuento(10);
+            bebidas[2].AplicarDescuento(5);
+            Console.WriteLine();
+            //Preparamos las bebidas
+            Console.WriteLine("Preparando Bebidas");
+            for (int i = 0; i < 3; i++)
+            {
+                bebidas[i].Preparar();
+            }
+            //Mostramos descrpcion de las bebidas
+            Console.WriteLine("Descripcion de Bebidas");           
+            for(int i = 0;i < 3; i++)
+            {
+                bebidas[i].MostrarDescripcion();
+            }
+
+
+            
+        }
+    }
+}
